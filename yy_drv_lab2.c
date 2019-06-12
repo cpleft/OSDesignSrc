4d7c0f1a5c79935514fbba1d3d5abb875ed854a1
ubuntu <429426523@qq.com>
2019-06-12 12:54:58 +0800

    auto update

--- yy_drv_lab2.c
+++ yy_drv_lab2.c
@@ -8,221 +8,157 @@
 #include<linux/seq_file.h>      
 #include<linux/proc_fs.h>     	/* proc_create(), remove_proc_entry() */
 
+#define VERSION "yy_drv_lab2"
+#define PROC_NAME "yy_proc"
+#define MAX_SIZE 1024
 
-#define  YY_MAJOR       98
-#define  VERSION        "yy_drv_lab2"
-#define  BUFFER_SIZE    1024
+static struct yy_proc_data {
+    char buf[MAX_SIZE];
+    struct yy_proc_data* next;
+};
+static loff_t yy_proc_data_size = 0;/* 记录数据链表中的结构体一共有几个 */
+static struct yy_proc_data* head = NULL;
+static struct yy_proc_data* tail = NULL;
 
-/* module param */
-int N = 512;
-module_param(N, int, 0);
-MODULE_PARM_DESC(N, "the size of the yy_contain buffer.");
-
-/* functions declaration */
-void showVersion(void);
-ssize_t yy_seq_write(struct file* fp, const char* buf, 
-		size_t count, loff_t* f_ops);
-ssize_t yy_seq_read(struct file* fp, char* buf, 
-        size_t count, loff_t* f_ops);
-int yy_seq_open(struct inode* ip, 
-        struct file* fp);
-int yy_seq_release(struct inode* ip, 
-        struct file* fp);
-
-
-static void add_one(void);
-static void* _seq_start(struct seq_file* seq_fp, loff_t* pos);
-static void* _seq_next(struct seq_file* seq_fp, void* np, loff_t* pos);
-static void _seq_stop(struct seq_file* seq_fp, void* np);
-static int _seq_show(struct seq_file* seq_fp, void* np);
-
-
-static struct mutex lock;
-static struct list_head head;
-struct yy_contain
+static void* yy_seq_start(struct seq_file* sfp, loff_t* pos)
 {
-    struct list_head list;
-    int count;
-    char buffer[BUFFER_SIZE];
-    int size;
-};
+    printk("yy_seq_start[--kernel--]\n");
 
-/* register */
-struct file_operations yy_f_ops = {
-    open   : yy_seq_open,
-    read   : seq_read,
-    write  : yy_seq_write,
-    release: yy_seq_release,
-};
-
-/* seq_file opeations */
-static struct seq_operations _seq_fops = {
-    .start = _seq_start,
-    .next = _seq_next,
-    .stop = _seq_stop,
-    .show = _seq_show
-};
-
-
-void showVersion(void)
-{
-    printk("****************************\n");
-    printk("\t%s\t\n", VERSION);
-    printk("****************************\n");
+    *pos = 0;
+    return head;
 }
 
-ssize_t yy_seq_read(struct file* fp, 
-        char* buf, 
-        size_t count, 
-        loff_t* f_ops)
+static void* yy_seq_next(struct seq_file* sfp, void* v, loff_t* pos)
 {
-    printk("yy_seq_read[--kernel--]\n");
-    
-    return 0;
+    printk("yy_seq_next[--kernel--]\n");
+
+    return v->next;
 }
 
-ssize_t yy_seq_write(struct file* fp, 
+static void yy_seq_show(struct seq_file* sfp, void* v)
+{
+    printk("yy_seq_show[--kernel--]\n");
+
+    if (v == NULL)
+        return;
+
+    seq_printf("%s(yy_seq_show)\n", v->buf);
+}
+
+static void yy_seq_stop(struct seq_file* sfp, void* v)
+{
+    return;
+}
+
+static struct seq_operations yy_seq_ops = {
+    .start = yy_seq_start,
+    .next = yy_seq_next,
+    .show = yy_seq_show,
+    .stop = yy_seq_stop
+};
+
+
+static int yy_open(struct inode* ip, struct file* fp)
+{
+    return seq_open(fp, &yy_seq_ops);
+}
+
+static ssize_t yy_write(
+        struct file* fp, 
         const char* buf, 
         size_t count, 
-        loff_t* f_ops)
+        loff_t* pos)
 {
-    printk("yy_seq_write[--kernel--]\n");
-
-    add_one();
-
-
-    return 0;
-}
-
-int yy_seq_open(struct inode* ip, 
-        struct file* fp)
-{
-    printk("yy_seq_open[--kernel--]\n");
-    return seq_open(fp, &_seq_fops);
-}
-
-int yy_seq_release(struct inode* ip, 
-        struct file* fp)
-{
-    printk("yy_seq_release[--kernel__]\n");
-    
-    /*? It seems data should be released in _seq_xxx functions, not here. */
-    struct yy_contain* yy_cp;
-    while (!list_empty(&head))
+    struct yy_proc_data* yy_proc_data_new = (struct yy_proc_data*)
+        kmalloc(sizeof(struct yy_proc_data), GFP_KERNEL);
+    if (yy_proc_data_new == NULL)
     {
-        yy_cp = list_entry(head.next, struct yy_contain, list);
-        list_del(&yy_cp->list);
-        kfree(yy_cp);
+        printk("kmalloc for yy_proc_data_new failed.\n");
+        return -1;
     }
 
-    return 0;
-}
+    int write_size = count;
+    if (count > MAX_SIZE)
+        write_size = MAX_SIZE;
 
-
-
-static void add_one(void)
-{
-    printk("add_one[--kernel__]\n");
-
-    struct yy_contain* yy_cp = NULL;
-
-    mutex_lock(&lock);
-    yy_cp = (struct yy_contain*)kmalloc(sizeof(struct yy_contain), GFP_KERNEL);
-    /* init */
-    int i = 1;
-    for (i = 0; i < BUFFER_SIZE; ++i)
+    if(copy_from_user(yy_proc_data_new->buf, buf, write_size) == 0)
     {
-        (*yy_cp).buffer[i] = 0;
+        printk("copy_from_user success.\n");
     }
-    (*yy_cp).size = N;
-    (*yy_cp).count = 1;
+    else
+    {
+        printk("copy_from_user failed.\n");
+        /* kfree() */
+        kfree(yy_proc_data_new);
+        return -1;
+    }
 
-    if (yy_cp != NULL)
-        list_add(&yy_cp->list, &head);
-    mutex_unlock(&lock);
-}
-
-static void* _seq_start(struct seq_file* seq_fp, loff_t* pos)
-{
-    printk("_seq_start[--kernel__]\n");
-    mutex_lock(&lock);
-    return seq_list_start(&head, *pos);
-}
-static void* _seq_next(struct seq_file* seq_fp, void* np, loff_t* pos)
-{
-    printk("_seq_next[--kernel__]\n");
-    return seq_list_next(np, &head, pos);
-}
-static void _seq_stop(struct seq_file* seq_fp, void* np)
-{
-    printk("_seq_stop[--kernel__]\n");
-    mutex_unlock(&lock);
-}
-static int _seq_show(struct seq_file* seq_fp, void* np)
-{
-    printk("_seq_show[--kernel__]\n");
-    struct yy_contain* yy_cp = list_entry(np, struct yy_contain, list);
-    seq_printf(seq_fp, "value: %s\n", yy_cp->buffer);
-	return 0;
+    yy_proc_data_new->next = NULL;
+    tail->next = yy_proc_data_new;
+    tail = yy_proc_data_new;
+    return write_size;
 }
 
+static struct file_operations yy_file_ops = {
+    .open = yy_open,
+    .read = seq_read,
+    .write = yy_write,
+    .release = seq_release
+};
 
-/* yy_drv init */
+
+/* yy_proc init */
 static int yy_init(void)
 {
-    int ret = -1;
-    ret = register_chrdev(YY_MAJOR, VERSION, &yy_f_ops);
-    showVersion();
-    if (ret < 0) {
-        printk("yy_drv register failed with %d[--kernel--]\n", ret);
-        return ret;
-    }
-    else {
-        printk("yy_drv register success![--kernel--]\n");
-    }
-    printk("\n...\nret=%x\n...\n", ret);
+    printk("init yy_proc[--kernel--]\n");	
 
-    /* init seq_file interface */
+    /* proc */
     struct proc_dir_entry* entry;
-
-    mutex_init(&lock);
-
-    INIT_LIST_HEAD(&head);
-    add_one(); add_one();
-
-    entry = proc_create("yy_data", S_IWUSR | S_IRUGO, NULL, &_seq_fops);
+    entry = proc_create(PROC_NAME, S_IWUSR | S_IRUGO, NULL, &yy_file_ops);
     if (entry == NULL)
     {
-		printk("failed to create proc file.\n");
-        // free add_one()'s space.
-        struct yy_contain* yy_cp;
-        while (!list_empty(&head))
-        {
-            yy_cp = list_entry(head.next, struct yy_contain, list);
-            list_del(&yy_cp->list);
-            kfree(yy_cp);
-        }
+        printk("proc_create failed.\n");
         return -ENOMEM;
     }
-    return ret;
-}
-
-/* yy_drv exit */
-static void yy_exit(void)
-{
-    printk("cleanup yy_drv[--kernel--]\n");	
+    printk("proc_create success.\n");
     
-    /* exit seq_file */
-    struct yy_contain* yy_cp;
-    remove_proc_entry("yy_data", NULL);
-    while (!list_empty(&head))
+    /* init data */
+    /* init a yy_proc_data */
+    struct yy_proc_data* yy_proc_data_new = (struct yy_proc_data*)
+        kmalloc(sizeof(struct yy_proc_data), GFP_KERNEL);
+    if (yy_proc_data_new == NULL)
     {
-        yy_cp = list_entry(head.next, struct yy_contain, list);
-        list_del(&yy_cp->list);
-        kfree(yy_cp);
+        printk("kmalloc for yy_proc_data_new failed.\n");
+        return -1;
     }
 
-    unregister_chrdev(YY_MAJOR, VERSION);
+    head = yy_proc_data_new;
+
+    yy_proc_data_new->buf[] = {
+        "hello, I'm from kernel space initialed in yy_init(void).\n"};
+    yy_proc_data_new->next = NULL;
+
+    tail = yy_proc_data_new;
+    return 0;
+}
+
+/* yy_proc exit */
+static void yy_exit(void)
+{
+    printk("cleanup yy_proc[--kernel--]\n");	
+
+    /* remove proc */
+    remove_proc_entry(PROC_NAME, NULL);
+
+    /* kfree data */
+    struct yy_proc_data* it = head;
+    while (it != NULL)
+    {
+        struct yy_proc_data* it_tmp = it;
+        it = it->next;
+        kfree(it_tmp);
+    }
+    return;
 }
 
 /* module info */
